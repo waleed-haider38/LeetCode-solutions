a088f90ea1dd8beb07e5b3127ddf8c444148ab58
waleed-haider38 <waleedhaider988@gmail.com>
2026-05-12 09:11:07 +0500

    Add solution of longest substring without repeating character using sliding window technique

--- longest_substring_without_repeating_character.py
+++ longest_substring_without_repeating_character.py
@@ -1,15 +1,21 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        char_set = set()
+        # 1. Setup tools
+        char_map = {} # Our Memory Notebook
         left = 0
-        max_length = 0
-
-        for right in range(len(s)):
-            while s[right] in char_set:
-                char_set.remove(s[left])
-                left += 1
-
-            char_set.add(s[right])
-            max_length = max(max_length, right - left + 1)
-
-        return max_length
+        max_len = 0
+        
+        # 2. Expand window with 'right' finger
+        for right, char in enumerate(s):
+            # 3. Conflict Check: Have we seen this letter before?
+            if char in char_map:
+                # 4. Jump the 'left' finger forward
+                left = max(left, char_map[char] + 1)
+            
+            # 5. Record/Update the most recent position
+            char_map[char] = right
+            
+            # 6. Update max_length of the current "clean" window
+            max_len = max(max_len, right - left + 1)
+            
+        return max_len
